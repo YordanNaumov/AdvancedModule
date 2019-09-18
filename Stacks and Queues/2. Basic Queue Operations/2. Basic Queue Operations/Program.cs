31fd8a49089a7cf990b0503b7a4bdd99e419233e
YordanNaumov <yordannaumov99@gmail.com>
2019-09-18 13:04:30 +0300

    Task 2 finished

---
+++ Stacks and Queues/2. Basic Queue Operations/2. Basic Queue Operations/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = Console
+               .ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToArray();
+
+            int elementsToEnqueue = elements[0];
+            int elementsToDequeue = elements[1];
+            int elementToSearch = elements[2];
+
+            int[] numbers = Console
+                .ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < elementsToEnqueue && i < numbers.Length; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < elementsToDequeue && queue.Count > 0; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Contains(elementToSearch))
+            {
+                Console.WriteLine("true");
+            }
+            else if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                GetSmallest(queue);
+            }
+        }
+
+        static void GetSmallest(Queue<int> queue)
+        {
+            int smallestNumber = int.MaxValue;
+
+            while (queue.Count > 0)
+            {
+                int currentNumber = queue.Peek();
+                if (smallestNumber > currentNumber)
+                {
+                    smallestNumber = currentNumber;
+                }
+                queue.Dequeue();
+            }
+            Console.WriteLine(smallestNumber);
+        }
+    }
+}
