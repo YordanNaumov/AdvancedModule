b621c6d8daf6615f5483c33bbb5fcfc1cd47246e
YordanNaumov <yordannaumov99@gmail.com>
2019-09-19 11:29:25 +0300

    Task 6 and 7

---
+++ Stacks and Queues/6.  Songs Queue/6.  Songs Queue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            var queueOfSongs = new Queue<string>(songs);
+
+            while (queueOfSongs.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Play")
+                {
+                    queueOfSongs.Dequeue();
+                }
+                else if (command.StartsWith("Add"))
+                {
+                    string song = command.Substring(4);
+                    if (queueOfSongs.Contains(song))
+                    {
+                        Console.WriteLine(song + " is already contained!");
+                    }
+                    else
+                    {
+                        queueOfSongs.Enqueue(song);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(", ", queueOfSongs));
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
---
+++ Stacks and Queues/7.  Truck Tour/7.  Truck Tour/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPumps = int.Parse(Console.ReadLine());
+            var petrolGiven = new List<int>();
+            var distanceToTravel = new List<int>();
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                int[] information = Console
+                    .ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                petrolGiven.Add(information[0]);
+                distanceToTravel.Add(information[1]);
+            }
+
+            bool pumpFound = false;
+            for (int currentPump = 0; currentPump < numberOfPumps; currentPump++)
+            {
+                var testPetrol = new Queue<int>(petrolGiven);
+                var testDistance = new Queue<int>(distanceToTravel);
+                int petrolTank = 0;
+                for (int i = currentPump; i < numberOfPumps + currentPump; i++)
+                {
+                    petrolTank += testPetrol.Peek();
+                    if (petrolTank >= testDistance.Peek())
+                    {
+                        petrolTank -= testDistance.Peek();
+                        testPetrol.Dequeue();
+                        testDistance.Dequeue();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    if (i == numberOfPumps + currentPump - 1)
+                    {
+                        pumpFound = true;
+                        Console.WriteLine(currentPump);
+                        break;
+                    }
+                }
+                if (pumpFound)
+                {
+                    break;
+                }
+                SwitchPump(petrolGiven);
+                SwitchPump(distanceToTravel);
+            }
+        }
+
+        static List<int> SwitchPump(List<int> list)
+        {
+            list.Add(list[list.Count - 1]);
+            list.RemoveAt(0);
+            list.TrimExcess();
+            return list;
+        }
+    }
+}
