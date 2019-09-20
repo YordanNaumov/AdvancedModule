a6a764561298add692b17ab9464d6b937bff95c0
YordanNaumov <yordannaumov99@gmail.com>
2019-09-20 15:55:16 +0300

    Task 8 and 9 almost complete

---
+++ Stacks and Queues/8.  Balanced Parentheses/8.  Balanced Parentheses/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string input = Console.ReadLine();
+            if (input.Length % 2 == 0)
+            {
+                var queue = new Queue<char>();
+                var alternativeStartQueue = new Queue<char>();
+                var alternativeEndQueue = new Queue<char>();
+                var stack = new Stack<char>();              
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] == '{' || input[i] == '[' || input[i] == '(')
+                    {
+                        queue.Enqueue(input[i]);
+                        alternativeStartQueue.Enqueue(input[i]);
+                    }
+                    else if (input[i] == '}' || input[i] == ']' || input[i] == ')')
+                    {
+                        stack.Push(input[i]);
+                        alternativeEndQueue.Enqueue(input[i]);
+                    }
+                }
+
+                if (queue.Count == input.Length / 2 && stack.Count == input.Length / 2)
+                {
+                    bool allValid = false;
+                    for (int i = 0; i < input.Length / 2; i++)
+                    {
+                        int possibleDifference = 2;
+                        if (queue.Peek() == 40)
+                        {
+                            possibleDifference = 1;
+                        }
+
+                        if (queue.Peek() + possibleDifference == stack.Peek())
+                        {
+                            queue.Dequeue();
+                            stack.Pop();
+                        }
+                        else
+                        {
+                                break;
+                        }
+
+                        if (queue.Count == 0)
+                        {
+                            Console.WriteLine("YES");
+                            allValid = true;
+                            break;
+                        }
+                    }
+
+                    for (int i = 0; i < input.Length && allValid == false; i++)
+                    {
+                        int possibleDifference = 2;
+                        if (alternativeStartQueue.Peek() == 40)
+                        {
+                            possibleDifference = 1;
+                        }
+
+                        if (alternativeStartQueue.Peek() + possibleDifference == alternativeEndQueue.Peek())
+                        {
+                            alternativeStartQueue.Dequeue();
+                            alternativeEndQueue.Dequeue();
+                        }
+                        else
+                        {
+                            Console.WriteLine("NO");
+                            break;
+                        }
+
+                        if (alternativeStartQueue.Count == 0)
+                        {
+                            Console.WriteLine("YES"); 
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ Stacks and Queues/9.  Simple Text Editor/9.  Simple Text Editor/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _9.Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            string currentText = string.Empty;
+            var previousText = new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+                int operationType = int.Parse(command[0]);
+
+                switch (operationType)
+                {
+                    case 1:
+                        previousText.Add(currentText);
+                        currentText += command[1];
+                        break;
+                    case 2:
+                        int count = currentText.Length - int.Parse(command[1]);
+                        previousText.Add(currentText);
+                        currentText = currentText.Remove(count);
+                        break;
+                    case 3:
+                        int position = int.Parse(command[1]) - 1;
+                        if (position <= currentText.Length)
+                        {
+                            Console.WriteLine(currentText[position]);
+                        }
+                        else
+                        {
+                            Console.WriteLine(currentText[currentText.Length-1]);
+                        }
+                        break;
+                    case 4:
+                        if (previousText.Count > 0)
+                        {
+                            currentText = previousText[previousText.Count - 1];
+                            previousText.RemoveAt(previousText.Count - 1);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
