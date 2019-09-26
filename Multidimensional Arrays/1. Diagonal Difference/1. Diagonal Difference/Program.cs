3822b63278625961c4d4417a8cc5fb95db6706dc
YordanNaumov <yordannaumov99@gmail.com>
2019-09-26 17:16:55 +0300

    Task 1

---
+++ Multidimensional Arrays/1. Diagonal Difference/1. Diagonal Difference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] colElements = Console
+                    .ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int primaryDiagonal = 0;
+            int secondaryDiagonal = 0;
+
+            for (int i = 0, p = matrix.GetLength(0) - 1; i < matrix.GetLength(0); i++, p--)
+            {
+                primaryDiagonal += matrix[i, i];
+                secondaryDiagonal += matrix[p, i];
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
+        }
+    }
+}
