16687af3a086a8c087d7bec3adad6bef89a1f71b
YordanNaumov <yordannaumov99@gmail.com>
2019-10-05 11:42:21 +0300

    Task 1 and 2

---
+++ Sets and Dictionaries Advanced/01. Unique Usernames/01. Unique Usernames/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            var names = new List<string>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (!names.Contains(input))
+                {
+                    names.Add(input);
+                }
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced/2. Sets of Elements/2. Sets of Elements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var firstSet = new HashSet<int>();
+            var secondSet = new HashSet<int>();
+            var setToPrint = new HashSet<int>();
+            var list = new List<int>();
+
+            for (int i = 0; i < inputNumbers[0] + inputNumbers[1]; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine())); 
+            }
+
+            for (int i = inputNumbers[0]; i < inputNumbers[0] + inputNumbers[1]; i++)
+            {
+                secondSet.Add(list[i]);
+            }
+
+            for (int i = 0; i < inputNumbers[0]; i++)
+            {
+                int number = list[i];
+                firstSet.Add(number);
+
+                if (firstSet.Contains(number) && secondSet.Contains(number))
+                {
+                    setToPrint.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", setToPrint));
+        }
+    }
+}
