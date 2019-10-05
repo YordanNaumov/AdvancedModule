4f9d0afa6aeb0b7b2ac3152b5079016912e69a4c
YordanNaumov <yordannaumov99@gmail.com>
2019-10-05 19:12:20 +0300

    Task 5 and 6

---
+++ Sets and Dictionaries Advanced/5. Count Symbols/5. Count Symbols/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var charAppearance = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (charAppearance.ContainsKey(input[i]))
+                {
+                    charAppearance[input[i]]++;
+                }
+                else
+                {
+                    charAppearance[input[i]] = 1;
+                }
+            }
+
+            charAppearance = charAppearance.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var @char in charAppearance)
+            {
+                Console.WriteLine($"{@char.Key}: {@char.Value} time/s");
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced/6. Wardrobe/6. Wardrobe/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new[] { " -> " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string color = input[0];
+
+                string[] items = input[1]
+                    .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe[color] = new Dictionary<string, int>();
+
+                    foreach (var item in items)
+                    {
+                        if (wardrobe[color].ContainsKey(item))
+                        {
+                            wardrobe[color][item]++;
+                        }
+                        else
+                        {
+                            wardrobe[color][item] = 1;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var item in items)
+                    {
+                        if (wardrobe[color].ContainsKey(item))
+                        {
+                            wardrobe[color][item]++;
+                        }
+                        else
+                        {
+                            wardrobe[color][item] = 1;
+                        }
+                    }
+                }
+            }
+
+            string[] finalCommand = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (var items in wardrobe)
+            {
+                Console.WriteLine($"{items.Key} clothes:");
+
+                bool colorFound = false;
+
+                if (finalCommand[0] == items.Key)
+                {
+                    colorFound = true;
+                }
+                foreach (var indinidualItem in items.Value)
+                {
+                    if (colorFound && indinidualItem.Key == finalCommand[1])
+                    {
+                        Console.WriteLine($"* {indinidualItem.Key} - {indinidualItem.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {indinidualItem.Key} - {indinidualItem.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
