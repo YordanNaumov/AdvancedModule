d923c8b4b5063a04568ce8a19f9bbf07485148ca
YordanNaumov <yordannaumov99@gmail.com>
2019-10-05 14:08:19 +0300

    Task 3 and 4

---
+++ Sets and Dictionaries Advanced/3. Periodic Table/Problem 3. Periodic Table/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3.Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            var set = new HashSet<string>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                foreach (var @string in input)
+                {
+                    set.Add(@string);
+                } 
+            }
+            List<string> list = new List<string>(set.OrderBy(x => x));
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced/4. Even Times/4. Even Times/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            var numberAppearance = new Dictionary<int, int>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (numberAppearance.ContainsKey(number))
+                {
+                    numberAppearance[number]++;
+                }
+                else
+                {
+                    numberAppearance[number] = 1;
+                }
+            }
+
+            foreach (var number in numberAppearance)
+            {
+                if (number.Value % 2 == 0)
+                {
+                    Console.WriteLine(number.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
