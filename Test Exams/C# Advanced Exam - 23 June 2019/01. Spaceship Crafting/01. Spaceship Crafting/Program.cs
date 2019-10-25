f4be9ad1f7ac791cc4e93e405f8d96d78c600a2d
YordanNaumov <yordannaumov99@gmail.com>
2019-10-25 17:00:18 +0300

    Test 1

---
+++ Test Exams/C# Advanced Exam - 23 June 2019/01. Spaceship Crafting/01. Spaceship Crafting/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Spaceship_Crafting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> liquids = new Queue<int>(Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            List<int> items = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            var physicalTable = new Dictionary<string, int>
+            {
+                ["Glass"] = 25,
+                ["Aluminium"] = 50,
+                ["Lithium"] = 75,
+                ["Carbon fiber"] = 100,
+            };
+
+            var collectedMaterials = new Dictionary<string, int>
+            {
+                ["Glass"] = 0,
+                ["Aluminium"] = 0,
+                ["Lithium"] = 0,
+                ["Carbon fiber"] = 0,
+            };
+
+            int uniqueMaterials = 0;
+
+            while (liquids.Count > 0 && items.Count > 0)
+            {
+                int sum = liquids.Peek() + items[0];
+                bool itemNotFound = true;
+
+                foreach (var item in physicalTable)
+                {
+                    if (sum == item.Value)
+                    {
+                        itemNotFound = false; 
+                        items.RemoveAt(0);
+
+                        if (collectedMaterials[item.Key] == 0)
+                        {
+                            uniqueMaterials++;
+                        }
+                        collectedMaterials[item.Key]++;
+
+                        break;
+                    }
+                }
+                liquids.Dequeue();
+
+                if (itemNotFound)
+                {
+                    items[0] += 3;
+                }
+            }
+
+            if (uniqueMaterials == 4)
+            {
+                Console.WriteLine("Wohoo! You succeeded in building the spaceship!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to build the spaceship.");
+            }
+
+            if (liquids.Count > 0)
+            {
+                Console.WriteLine("Liquids left: " + string.Join(", ", liquids));
+            }
+            else
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+
+            if (items.Count > 0)
+            {
+                Console.WriteLine("Physical items left: " + string.Join(", ", items));
+            }
+            else
+            {
+                Console.WriteLine("Physical items left: none");
+            }
+
+            collectedMaterials = collectedMaterials.OrderBy(x => x.Key).ToDictionary(x => x.Key, y => y.Value);
+
+            foreach (var material in collectedMaterials)
+            {
+                Console.WriteLine($"{material.Key}: {material.Value}");
+            }
+        }
+    }
+}
---
+++ Test Exams/C# Advanced Exam - 23 June 2019/02. Space Station Recruitment/SpaceStationRecruitment/SpaceStation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SpaceStationRecruitment
+{
+    class SpaceStation
+    {
+        private string name;
+        private int capacity;
+        private List<Astronaut> data;
+        public int Count => Data.Count();
+
+        public SpaceStation(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Data = new List<Astronaut>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        public List<Astronaut> Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public void Add(Astronaut astronaut)
+        {
+            if (Data.Count < capacity)
+            {
+                Data.Add(astronaut);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            foreach (var astronaut in Data)
+            {
+                if (astronaut.Name == name)
+                {
+                    Data.Remove(astronaut);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Astronaut GetOldestAstronaut()
+        {
+            Astronaut astronaut = Data.OrderBy(x => x.Age).First();
+
+            return astronaut;
+        }
+
+        public Astronaut GetAstronaut(string name)
+        {
+            Astronaut astronaut = Data.Where(x => x.Name == name).FirstOrDefault();
+
+            return astronaut;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Astronauts working at Space Station {Name}:");
+            foreach (var astronaut in data)
+            {
+                sb.AppendLine(astronaut.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
